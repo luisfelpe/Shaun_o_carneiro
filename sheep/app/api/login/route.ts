59dd67b739614d3b3804320f61e3f25fa5d82c4e
Luis Felipe Teobaldo <luis.teobaldo09@aluno.ifce.edu.br>
2026-09-11 12:59:31 +0000

    usuarios2

--- sheep/app/api/login/route.ts
+++ sheep/app/api/login/route.ts
@@ -6,10 +6,10 @@ export async function POST(request: Request) {
     const { username, password } = body;
 
     // Credenciais de exemplo — substitua por validação real
-    const validUser = username === 'Felps' && password === 'flipones2';
-    const validUser = username === 'Allana' && password === 'marques';
-    const validUser = username === 'Gabi' && password === 'rebecca';
-    const validUser = username === 'Raira' && password === 'alves';
+    const validUser =[[username === 'Felps' && password === 'flipones2'],
+    [username === 'Allana' && password === 'marques'],
+    [username === 'Gabi' && password === 'rebecca'],
+    [username === 'Raira' && password === 'alves']];
 
     if (!validUser) {
       return NextResponse.json({ ok: false, message: 'Credenciais inválidas' }, { status: 401 });
